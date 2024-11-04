b931b732e9d262074807c708e19e9eac4adbdb73
Mahad <mahad@xconn.io>
2024-11-04 18:59:55 +0500

    Fix CRA authenticate to include salt (#42)

--- src/test/kotlin/io/xconn/wampproto/interoperability/auth/CRAAuthenticatorTest.kt
+++ src/test/kotlin/io/xconn/wampproto/interoperability/auth/CRAAuthenticatorTest.kt
@@ -1,18 +1,28 @@
 package io.xconn.wampproto.interoperability.auth
 
+import io.xconn.wampproto.auth.CRAAuthenticator
 import io.xconn.wampproto.auth.generateWAMPCRAChallenge
 import io.xconn.wampproto.auth.signWampCRAChallenge
 import io.xconn.wampproto.auth.verifyWampCRASignature
 import io.xconn.wampproto.interoperability.runCommand
+import io.xconn.wampproto.messages.Challenge
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.text.trim
 
 class CRAAuthenticatorTest {
     private val sessionID = 123
-    private val authID = "john"
+    private val authID = "foo"
     private val authRole = "admin"
     private val provider = "provider"
     private val testSecret = "secret"
+    private val salt = "salt"
+    private val keylength = 32
+    private val iterations = 1000
+    private val craChallenge =
+        """{"nonce":"cdcb3b12d56e12825be99f38f55ba43f","authprovider":"provider",""" +
+            """"authid":"foo","authrole":"admin","authmethod":"wampcra","session":123,"timestamp":"2024-05-07T09:25:13.307Z"}"""
+    private val authExtra = mapOf("challenge" to craChallenge, "salt" to salt, "iterations" to iterations, "keylen" to keylength)
 
     @Test
     fun testGenerateChallenge() {
@@ -39,6 +49,28 @@ class CRAAuthenticatorTest {
         val signature = runCommand("wampproto auth cra sign-challenge ${challenge.trim()} $testSecret")
 
         val isVerified = verifyWampCRASignature(signature.trim(), challenge.trim(), testSecret.toByteArray(Charsets.UTF_8))
+        assertEquals(true, isVerified)
+    }
+
+    @Test
+    fun testSignWAMPCRASignatureWithSalt() {
+        val challenge = Challenge(CRAAuthenticator.TYPE, authExtra)
+        val authenticator = CRAAuthenticator(authID, authExtra, testSecret)
+        val authenticate = authenticator.authenticate(challenge)
+
+        val saltSecret = runCommand("wampproto auth cra derive-key $salt $testSecret -i $iterations -l $keylength")
+
+        runCommand("wampproto auth cra verify-signature $craChallenge ${authenticate.signature} $saltSecret")
+    }
+
+    @Test
+    fun testVerifyWAMPCRASignatureWithSalt() {
+        val challenge = runCommand("wampproto auth cra generate-challenge $sessionID $authID $authRole $provider")
+
+        val saltSecret = runCommand("wampproto auth cra derive-key $salt $testSecret -i $iterations -l $keylength")
+        val signature = runCommand("wampproto auth cra sign-challenge ${challenge.trim()} ${saltSecret.trim()}")
+
+        val isVerified = verifyWampCRASignature(signature.trim(), challenge.trim(), saltSecret.trim().encodeToByteArray())
         assertEquals(true, isVerified)
     }
 }
